1af09d690c63ff52ece45be3eeedfff7cc97c12b
julioOsorio <jotalvaro.cc@gmail.com>
2022-05-02 22:30:41 -0500

    agregando factura-mascota

--- src/app/feature/mascota/shared/service/mascota/mascota.service.ts
+++ src/app/feature/mascota/shared/service/mascota/mascota.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { HttpService } from '@core/services/http.service';
 import { environment } from 'src/environments/environment';
-import { Mascota } from '../model/mascota';
+import { Mascota } from '@mascota/shared/model/mascota';
 
 @Injectable({
   providedIn: 'root'
