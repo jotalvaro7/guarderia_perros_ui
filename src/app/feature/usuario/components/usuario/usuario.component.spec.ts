b74f806ce0599063457d88b8aacd23a1675a733b
julioOsorio <jotalvaro.cc@gmail.com>
2022-05-06 23:54:07 -0500

    refactorizando los test

--- src/app/feature/usuario/components/usuario/usuario.component.spec.ts
+++ src/app/feature/usuario/components/usuario/usuario.component.spec.ts
@@ -1,3 +1,4 @@
+import { of } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
@@ -8,11 +9,24 @@ import { MaterialModule } from '@shared/material/material-module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UsuarioService } from '@usuario/shared/service/usuario.service';
 import { UsuarioComponent } from './usuario.component';
+import { Usuario } from '@usuario/shared/model/usuario';
 
 
 describe('UsuarioComponent', () => {
   let component: UsuarioComponent;
   let fixture: ComponentFixture<UsuarioComponent>;
+
+  let usuarioService: UsuarioService;
+
+  let usuarios: Usuario[] = [
+    {
+    id: 1,
+    nombre: 'Julio' , 
+    apellido: 'Osorio', 
+    identificacion: '103694987', 
+    numeroCelular: '34725812'
+    }
+  ];
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -33,6 +47,8 @@ describe('UsuarioComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UsuarioComponent);
     component = fixture.componentInstance;
+    usuarioService = TestBed.inject(UsuarioService);
+    spyOn(usuarioService, 'consultar').and.returnValue(of(usuarios));
     fixture.detectChanges();
   });
 
